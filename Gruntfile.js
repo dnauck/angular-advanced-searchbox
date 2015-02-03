4491cb9fd61c1c302e95f26ecc61f85a454b8356
Daniel Nauck <d.nauck@nauck-it.de>
2015-02-03 16:22:01 +0100

    Embed template into final build
    * updated build process to create a to js converted and minified template that is added to the directives $templateCache
    * updated the build process to provide a distribution with normal and minified versions of the directive with or without the included template (like angular-ui-bootstrap)
    * added new dev dependencies to package.json
    * adjust path to main file in bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,14 +8,68 @@ module.exports = function(grunt) {
       ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> <%= pkg.author.url %>\n' +
       ' * License: <%= pkg.license %>\n' +
       ' */\n',
-
+    jshint: {
+      all: [
+        './src/*.js', '*.js', '*.json'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    ngtemplates: {
+      options: {
+        module: 'angular-advanced-searchbox',
+        htmlmin: {
+          collapseBooleanAttributes:      true,
+          collapseWhitespace:             true,
+          removeAttributeQuotes:          true,
+          removeComments:                 true, // Only if you don't use comment directives!
+          removeEmptyAttributes:          true,
+          removeRedundantAttributes:      true,
+          removeScriptTypeAttributes:     true,
+          removeStyleLinkTypeAttributes:  true
+        }
+      },
+      template: {
+        src: ['src/*.html'],
+        dest: 'tmp/templates.js'
+      },
+    },
+    concat: {
+      template: {
+        options: {
+        },
+        src: ['src/<%= pkg.name %>.js', '<%= ngtemplates.template.dest %>'],
+        dest: 'dist/<%= pkg.name %>-tpls.js'
+      }
+    },
+    copy: {
+      main : {
+        files: [
+          {
+            src: ['src/<%= pkg.name %>.js'], 
+            dest: 'dist/<%= pkg.name %>.js'
+          }
+        ]
+      },
+      template : {
+        files: [
+          {
+            src: ['src/<%= pkg.name %>.html'], 
+            dest: 'dist/<%= pkg.name %>.html'
+          }
+        ]
+      }
+    },
     uglify: {
       options: {
         banner: '<%= banner %>'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['dist/<%= pkg.name %>.js'],
+          'dist/<%= pkg.name %>-tpls.min.js': ['dist/<%= pkg.name %>-tpls.js'],
+        }
       }
     },
     cssmin: {
@@ -25,36 +79,39 @@ module.exports = function(grunt) {
       },
       minify: {
         src: 'src/<%= pkg.name %>.css',
-        dest: 'build/<%= pkg.name %>.min.css'
+        dest: 'dist/<%= pkg.name %>.min.css'
       }
     },
-    copy: {
-      template : {
-        files: [
-          {
-            src: ['src/angular-advanced-searchbox.html'], 
-            dest: 'build/angular-advanced-searchbox.html'
-          }
-        ]
+    clean: {
+      temp: {
+        src: [ 'tmp' ]
       }
     },
-    jshint: {
-      all: [
-        './src/*.js', '*.json'
-      ],
+    bump: {
       options: {
-        jshintrc: '.jshintrc'
+        files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
+        commit: true,
+        commitFiles: ['-a'],
+        createTag: true,
+        push: true,
+        pushTo: 'origin'
       }
     }
   });
 
   // Load the plugins
+  grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-angular-templates');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint:all', 'uglify', 'cssmin', 'copy']);
+  grunt.registerTask('default', ['clean', 'jshint:all', 'ngtemplates', 'concat', 'copy', 'uglify', 'cssmin']);
 
 };
